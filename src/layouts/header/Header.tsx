cbf8ec7810dbc588b2cea7e8d5453e5769e43e48
alexandr-obraztsov <alexlove2005@mail.ru>
2024-09-18 01:39:53 +0300

    Init project

---
+++ src/layouts/header/Header.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import styled from "styled-components";
+
+type HeaderPropsType = {
+
+};
+
+
+export const Header : React.FC<HeaderPropsType> = (props) => {
+    return (
+        <StyledHeader>
+
+        </StyledHeader>
+    );
+};
+
+
+const StyledHeader = styled.header`
+    padding: 12px 80px;
+    
+`
